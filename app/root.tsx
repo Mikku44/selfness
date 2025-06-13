d4016aa5610be7edb3773440725e516936f9050a
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2025-06-13 21:04:58 +0700

    Add Google Tag Manager integration to enhance tracking capabilities

--- app/root.tsx
+++ app/root.tsx
@@ -29,10 +29,29 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+         {/* Google Tag Manager (ส่วนแรก - ใน <head>) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NGQ54J3S');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <Meta />
         <Links />
       </head>
       <body>
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NGQ54J3S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <CookieConsent />
         {children}
         <ScrollRestoration />
